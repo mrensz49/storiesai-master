14e2ba0d50e81f0d4fde5d2cff4fbdafaf155c75
rene_sanguenza <rene@venturedrake.com>
2024-01-30 14:24:00 +0800

    debugging fb share

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://storiesforyou.net/sw.js', { scope: 'https://storiesforyou.net/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
